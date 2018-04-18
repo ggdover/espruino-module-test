1d13c34879011b073915078d67551800d6551cdf
Filip Doversten <filip@doversten.se>
2018-04-18 19:52:42 +0200

    Created simple custom module

---
+++ test_module.js
@@ -0,0 +1,13 @@
+//---------------------------------------------
+module.exports = {
+
+  // List of testable functions
+
+  addition : function(num1, num2) {
+    return num1 + num2;
+  },
+
+  subtraction : function(num1, num2) {
+    return num1 - num2;
+  }
+}
